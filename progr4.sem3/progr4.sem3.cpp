f67d09a2e49e9e254a6ad2884930194914004c21
Bedarev <bedarev.axl@mail.ru>
2020-11-21 15:39:42 +0700

    Создание каркаса программы

--- progr4.sem3/progr4.sem3.cpp
+++ progr4.sem3/progr4.sem3.cpp
@@ -1,6 +1,50 @@
 ﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class nation {
+public:
+    void Read() {
+        std::cout << "Введите название государства:";
+        std::cin >> title;
+        std::cout << "Введите кол-во населения:";
+        std::cin >> population;
+        std::cout << "Введите военную мощь (=<2):";
+        std::cin >> millitarypow;
+    }
+   /* void Init();
+    void Display();
+    void surrend();
+    void war();
+    void endwar();*/
+    std::string title;
+    int population;
+    float millitarypow;
+};
+
+class peacefull : public nation {
+public:
+    void devpopulation() {
+        population *= 1, 5;
+        millitarypow -= 0, 1;
+    };
+};
+
+class enemy : public nation {
+public:
+    void devmilitar() {
+        millitarypow += 1;
+        population *= 0, 7;
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "RUS");
+    peacefull *one = new peacefull();
+    enemy* two = new enemy();
+    one->Read();
+    two->Read();
+   
+
 }
